ff8900906835a12980e83bac881b7903ee2d7d3c
Pedro <60669964+pedro-henrique-araujo@users.noreply.github.com>
2023-09-12 19:04:15 +0000

    cause test error

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  calculatePercentage(fraction, total) {
+  calculatePercentage(fractions, total) {
     if (!total) return NaN;
     return Math.round((fraction / total) * 10000) / 100;
   },
